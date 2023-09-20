7ef2052403a88d5b8496eee059dd469a4e9110b7
lucette25 <hermione.odjo@imsp-uac.org>
2023-09-20 21:39:37 +0200

    first commit

---
+++ client/src/components/CustomButton.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import state from '../store'
+import { useSnapshot } from 'valtio'
+ const CustomButton = ({type,title,customStyles,handleClick}) => {
+    const snap=useSnapshot(state);
+    const generateStyle=(type)=>{
+        if(type=='filled'){
+            return {
+                backgroundColor:snap.color,             
+                color: '#fff'
+            }
+        }
+    }
+    return (
+    <button
+    className={`px-2 py-1.5 rounded-me ${customStyles}`}
+    style={generateStyle(type)}
+    onClick={handleClick}
+    >
+
+    </button>
+  )
+}
+export default CustomButton;
---
+++ client/src/pages/Customizer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+// Votre composant Customizer
+const Customizer = () => {
+    return (
+        <div>Customizer</div>
+      )  };
+  
+export default Customizer;
+
